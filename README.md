20698cdd892e6d583152f74b6f313dfb2e51a738
Manu Ramirez <manu.ramirez@espressive.com>
2021-04-26 15:38:39 -0500

    chore(cortado): improvements to Cortado:
    - save config to localStorage
    - extract constants to a constants file
    - update README.md

--- README.md
+++ README.md
@@ -4,14 +4,41 @@
 
 This logging tool is here to provide us with better debugging, especially when debugging in production environments. It provides component namespacing, different logging types, color codes, filters and other utils.
 
-
 ## Installation
 
 No installation steps are required when using this tool. The logger is ready in the global scope for it to use anywhere in the app.
 
 ## Usage
 
-By default, our logger will not log messages unless specified otherwise using `cortado.on()` (more details below).
+By default, our logger will not log messages unless specified otherwise using `cortado.on()` or via the configuration stored in `localStorage` (more details below).
+
+### Log types
+
+There are many types of log:
+
+- method
+- handler
+- hxr
+- ws
+- debug
+- DEPRECATED lifecycle
+- hook
+
+To better decouple these, you can import the available types from the same package:
+
+```js
+import { LOG_TYPE } from "esp-util-cortado";
+
+cortado.log({
+  type: LOG_TYPE.DEBUG,
+  label: "MyComponent",
+  message: "This is a sample debug message",
+});
+/*
+Output:
+[MyComponent] This is a sample debug message
+*/
+```
 
 To start logging messages to the console you use the logger object in the global scope:
 
@@ -22,30 +49,30 @@ Output:
 [Global] No message specified
 */
 
-cortado.log('This is a sample message')
+cortado.log("This is a sample message");
 /*
 Output:
 [Global] This is a sample message
 */
 
-cortado.log({message: 'This is another sample message'})
+cortado.log({ message: "This is another sample message" });
 /*
 Output:
 [Global] This is another sample message
 */
 ```
 
-Namespace is defaulted to `Global` if no label is especified to the cortado.
+If no namespace is specified, `Global` is infered.
 
-To namespace your logs you have to pass a string to the `label` property of the object you send. Passing an object to the logger provides the message with more information.
-
+To namespace your logs, you have to pass a string to the `label` property of the object you send. Passing an object to the logger provides the message with more information.
 
 ```js
+import { LOG_TYPE } from "esp-util-cortado";
+
 cortado.log({
-  type: 'debug',
-  label: 'MyComponent',
-  message: 'This is a sample message'
-})
+  label: "MyComponent",
+  message: "This is a sample message",
+});
 /*
 Output:
 [MyComponent] This is a sample message
@@ -57,6 +84,7 @@ Since we are passing a specific `type` too, the log will be color coded.
 ### Types
 
 We currently have these types in the logger:
+
 <ul>
   <li style="color: #3671d1">method</li>
   <li style="color: #33b752">handler</li>
@@ -75,28 +103,33 @@ cortado.xhr({'This logs a xhr message'});
 
 Follow the same syntax for the rest of the types. If `cortado.log()` uis used and no type is specified it'll default to `debug`.
 
-
 ## Utils
 
 The logger has a few util functions available to toggle the messages it shows.
 
 #### .on()
+
 Turns logger silent mode off, causing it to start logging messages.
 
 #### .off()
+
 Turns the logger silent mode on.
 
-#### .persist()
+#### DEPRECATED .persist()
+
 Turns logger silent mode off and saves it to localStorage so it can continue to log after navigating in the browser
 
-#### .halt()
+#### DEPRECATED .halt()
+
 Turns logger silent mode on and stops it from persisting across pages.
 
 ## Timers
+
 The logger has a timer utility you can turn on, restart, or delete for each namespace.
 
 #### trace
-When logging a message, if you specify `trace: true`.  A new timer will automatically get started.
+
+When logging a message, if you specify `trace: true`. A new timer will automatically get started.
 
 ```js
 cortado.debug({
@@ -118,21 +151,23 @@ cortado.debug({
 ```
 
 #### .addTimer(label)
+
 Adds a new timer for the specified namespace.
 
 #### .getTimer(label)
+
 Prints the current time for the specified namespace:
 
 ```js
-cortado.getTimer('MyComponent')
+cortado.getTimer("MyComponent");
 /*
  [MyComponent] Current timer: 152323ms 
 */
 ```
 
 #### .removeTimer(label)
-Removes the timer for a specified namespace. If a message from the same namespace gets logger with `trace: true`, a new timer will get started
 
+Removes the timer for a specified namespace. If a message from the same namespace gets logger with `trace: true`, a new timer will get started
 
 ## Filters
 
@@ -141,55 +176,54 @@ Logger enables you to filter logged messages by namespace or type. Filters are i
 Filter arrays are public in case you need to do more complex manipulations.
 
 #### .addNamespaceFilter(string)
+
 Adds a filter for namespace:
 
 ```js
-cortado.addNamespaceFilter('Component B');
+cortado.addNamespaceFilter("Component B");
 
-cortado.debug({label:'Component A', message: 'sample message'});
-cortado.debug({label:'Component B', message: 'sample message'});
+cortado.debug({ label: "Component A", message: "sample message" });
+cortado.debug({ label: "Component B", message: "sample message" });
 
 /*
 Output will only log Component B log:
 [Component B] sample message
 */
-
 ```
 
 #### .addTypeFilter(string)
+
 Works similar to namespace filter:
 
 ```js
-cortado.addTypeFilter('xhr');
+cortado.addTypeFilter("xhr");
 
-cortado.debug('debug message');
-cortado.method('method message');
-cortado.xhr('xhr message');
-cortado.ws('ws message');
-
+cortado.debug("debug message");
+cortado.method("method message");
+cortado.xhr("xhr message");
+cortado.ws("ws message");
 
 /*
 Output will only log Component B log:
 [Global] xhr message
 */
-
 ```
 
 You can also have multiple filter and mix them up with namespace filters.
 
 #### .clearNamespaceFilters(), .clearTypeFilters(), .clearAllFilters()
+
 Pretty obvious :)
 
-
-
 ## Production
+
 The logger can accept a `private` property to prevent a message from displaying in production environments, even if `.showAll()` has been called.
 
 ```js
 cortado.xhr({
-  message: 'This is a secret message',
-  private: true
-})
+  message: "This is a secret message",
+  private: true,
+});
 ```
 
 This message wouldn't show in production under any circumstances.
--- src/Cortado.js
+++ src/Cortado.js
@@ -1,72 +1,95 @@
+import { STATUS, LOG_TYPE, LOG_TYPE_COLOR, DEFAULT_LOG } from "./constants";
+
+const initialState = {
+  status: STATUS.OFF,
+  typeFilter: [],
+  namespaceFilter: [],
+};
+
 class Cortado {
-  silent = !localStorage.getItem('turnLoggerOn');
-  isProduction = process.env.NODE_ENV === 'production';
-  timers = {};
-  typeFilter = [];
-  namespaceFilter = [];
-  types = ['method', 'handler', 'xhr', 'ws' , 'debug', 'lifecycle'];
-  colors = {
-    method    : '#3671d1',
-    handler   : '#33b752',
-    xhr       : '#f2db0c',
-    ws        : '#29b9f2',
-    debug     : '#afafaf',
-    lifecycle : '#ffad3a',
-  }
-  defaultLog = {
-    label   : 'Global',
-    message : 'No message specified',
-    type    : 'debug',
-    trace   : false,
-  }
-
   constructor() {
-    this.shorthandFactory();
-  }
+    let stateFromLocalStorage = localStorage.getItem("ESPRESSIVE_CORTADO");
+    if (stateFromLocalStorage && typeof stateFromLocalStorage === "string") {
+      stateFromLocalStorage = JSON.parse(stateFromLocalStorage);
+    }
 
-  shorthandFactory = () => {
-    this.types.forEach((type) => {
+    this.state = stateFromLocalStorage || initialState;
+    this.isProduction = process.env.NODE_ENV === "production";
+    this.timers = {};
+
+    // generate shothand methods
+    Object.values(LOG_TYPE).forEach((type) => {
       this[type] = (info) => {
-        if (typeof info === 'string') {
-          return this.log({message: info,
-            type});
+        if (typeof info === "string") {
+          return this.log({ message: info, type });
         }
-        return this.log({...info,
-          type});
+        return this.log({ ...info, type });
       };
     });
   }
 
   // Turn Logger on and off
   on() {
-    this.silent = false;
+    this.persistStateToLocalStorage({
+      ...this.state,
+      status: STATUS.ON,
+    });
   }
+
   off() {
-    this.silent = true;
+    this.persistStateToLocalStorage({
+      ...this.state,
+      status: STATUS.OFF,
+    });
   }
 
-  persist() {
-    localStorage.setItem('turnLoggerOn', true);
-    this.on();
-  }
+  // we are removing these...
+  // persist() {
+  //   localStorage.setItem('turnLoggerOn', true);
+  //   this.on();
+  // }
 
-  halt() {
-    localStorage.removeItem('turnLoggerOn');
-    this.off();
-  }
+  // halt() {
+  //   localStorage.removeItem('turnLoggerOn');
+  //   this.off();
+  // }
 
   // Filters
 
-  addNamespaceFilter = (namespace) => this.namespaceFilter.push(namespace);
+  addNamespaceFilter = (namespace) => {
+    this.persistStateToLocalStorage({
+      ...this.state,
+      namespaceFilter: [...this.state.namespaceFilter, namespace],
+    });
+  };
 
-  addTypeFilter = (type) => this.typeFilter.push(type);
+  addTypeFilter = (type) => {
+    this.persistStateToLocalStorage({
+      ...this.state,
+      typeFilter: [...this.state.typeFilter, type],
+    });
+  };
 
-  clearNamespaceFilters = () => this.namespaceFilter = [];
+  clearNamespaceFilters = () => {
+    this.persistStateToLocalStorage({
+      ...this.state,
+      namespaceFilter: [],
+    });
+  };
 
-  clearTypeFilters = () => this.typeFilter = [];
+  clearTypeFilters = () => {
+    this.persistStateToLocalStorage({
+      ...this.state,
+      typeFilter: [],
+    });
+  };
 
   clearAllFilters = () => {
-    this.clearTypeFilters(); this.clearNamespaceFilters();
+    this.persistStateToLocalStorage({
+      ...this.state,
+      typeFilter: [],
+      namespaceFilter: [],
+    });
   };
 
   isPassingFilter = (filters, value) => {
@@ -74,69 +97,69 @@ class Cortado {
       return filters.indexOf(value) !== -1;
     }
     return true;
-  }
+  };
 
-  isPrivate = (priv) => priv && this.isProduction
+  isPrivate = (priv) => priv && this.isProduction;
 
   filterMessage = (info) => {
-    const {isPassingFilter, isPrivate, namespaceFilter, silent, typeFilter} = this;
+    const { isPassingFilter, isPrivate, state } = this;
 
-    return !silent &&
-            isPassingFilter(namespaceFilter, info.label) &&
-            isPassingFilter(typeFilter, info.type) &&
-            !isPrivate(info.private);
-  }
+    return (
+      state.status === STATUS.ON &&
+      isPassingFilter(state.namespaceFilter, info.label) &&
+      isPassingFilter(state.typeFilter, info.type) &&
+      !isPrivate(info.private)
+    );
+  };
 
   // Timers
   addTimer = (label) => {
-    const {timers} = this;
+    const { timers } = this;
     if (timers.hasOwnProperty(label)) {
-      const end = (new Date).getTime();
+      const end = new Date().getTime();
       const diff = end - timers[label];
       timers[label] = end;
-      return diff + 'ms';
+      return diff + "ms";
+    } else {
+      timers[label] = new Date().getTime();
+      return "New timer started";
     }
-    else {
-      timers[label] = (new Date).getTime();
-      return 'New timer started';
-    }
-
-    return null;
-  }
+  };
 
   getTimer = (label) => {
     this.log({
-      label   : label,
-      message : `Current timer: ${this.timers[label]}ms`,
+      label: label,
+      message: `Current timer: ${this.timers[label]}ms`,
     });
-  }
+  };
 
   removeTimer = (label) => {
-    const {timers} = this;
+    const { timers } = this;
     if (label in timers) {
       delete timers[label];
     }
-  }
+  };
 
   // Formatting and printing
-  formatDefault = (info) => {
-    const {defaultLog} = this;
-    return typeof info === 'string' ? {...defaultLog,
-      message: info} : {...defaultLog,
-      ...info};
-  }
+  formatDefault = (info) =>
+    typeof info === "string"
+      ? { ...DEFAULT_LOG, message: info }
+      : { ...DEFAULT_LOG, ...info };
+
   formatter = (info) => {
-    const {label, message, trace} = info;
-    return `%c[${label}] ${message} ${trace ? '- ' + this.addTimer(label) : ''}`;
-  }
+    const { label, message, trace } = info;
+    return `%c[${label}] ${message} ${
+      trace ? "- " + this.addTimer(label) : ""
+    }`;
+  };
 
   colorize = (type) => {
-    const color = this.colors[type] || this.colors.debug;
+    const color = LOG_TYPE_COLOR[type] || LOG_TYPE_COLOR.debug;
     return `color: ${color}`;
-  }
+  };
 
   log = (info) => {
-    const {colorize, formatter, formatDefault} = this;
+    const { colorize, formatter, formatDefault } = this;
 
     info = formatDefault(info);
 
@@ -144,8 +167,13 @@ class Cortado {
       // eslint-disable-next-line no-console
       console.log(formatter(info), colorize(info.type));
     }
-  }
+  };
 
+  persistStateToLocalStorage = (newState) => {
+    localStorage.setItem("ESPRESSIVE_CORTADO", JSON.stringify(newState));
+
+    this.state = newState;
+  };
 }
 
 export default Cortado;
